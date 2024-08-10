c4ce352d0eb7b7b20a95cef9be7ddd477fc2d872
nionoku <nionoku@gmail.com>
2024-08-10 16:25:44 +0300

    Add select other watermark pattern from list

--- src/components/watermark-elements-list-tool/watermark-elements-list-tool.vue
+++ src/components/watermark-elements-list-tool/watermark-elements-list-tool.vue
@@ -4,15 +4,16 @@
       Add watermark element
     </button>
 
-    <WatermarkElementsList :elements="elements" />
+    <WatermarkElementsList :elements="elements" @item-click="whenItemClick" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import styles from './index.module.css'
 import { storeToRefs } from 'pinia';
 import { useWatermarkElementsStore } from '../../stores/watermark-elements';
 import WatermarkElementsList from '../watermark-elements-list/watermark-elements-list.vue'
-import styles from './index.module.css'
+import { WatermarkElement } from '../../stores/watermark-elements/types/watermark-element';
 
 const { appendElement, setSelectedElement } = useWatermarkElementsStore()
 const { elements } = storeToRefs(useWatermarkElementsStore())
@@ -26,6 +27,10 @@ const whenClick = () => {
     scale: [1, 1]
   })
 
+  setSelectedElement(element)
+}
+
+const whenItemClick = (element: WatermarkElement) => {
   setSelectedElement(element)
 }
 
--- src/components/watermark-elements-list/watermark-elements-list.vue
+++ src/components/watermark-elements-list/watermark-elements-list.vue
@@ -1,6 +1,6 @@
 <template>
   <ul>
-    <li v-for="elem in elements" :key="elem.id">
+    <li v-for="elem in elements" :key="elem.id" @click="whenItemClick(elem)" :class="[styles.item]">
       <span>
         {{ elem.id }}
       </span>
@@ -19,5 +19,13 @@ import styles from './index.module.css'
 defineProps<{
   elements: WatermarkElement[]
 }>();
+
+const emits = defineEmits<{
+  itemClick: [element: WatermarkElement]
+}>()
+
+const whenItemClick = (element: WatermarkElement) => {
+  emits('itemClick', element)
+}
 
 </script>
