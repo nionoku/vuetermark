9853a4ba6cda3ce0314ba837fcf68abfac01af41
nionoku <nionoku@gmail.com>
2024-08-10 11:49:55 +0300

    Init app

---
+++ src/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <Editor :photo="ExamplePhoto" />
+</template>
+
+<script lang="ts" setup>
+import Editor from './components/editor/index.vue'
+import ExamplePhoto from './assets/example.jpg'
+
+const props = defineProps<{
+}>();
+
+const emits = defineEmits({
+});
+
+</script>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './assets/styles/index.css'
+
+import { createApp } from 'vue'
+import App from './app.vue'
+
+createApp(App)
+  .mount('#app')
