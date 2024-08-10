0284443897ca0b6e10968728dafef5b63b647999
nionoku <nionoku@gmail.com>
2024-08-10 16:17:10 +0300

    Add close element editor handler

--- src/components/watermark-element-tool/watermark-element-tool.vue
+++ src/components/watermark-element-tool/watermark-element-tool.vue
@@ -1,9 +1,16 @@
 <template>
   <div :class="[styles.container]">
-    <LabelControl v-model="props.selectedElement.label" />
-    <PositionControl v-model="props.selectedElement.position" />
-    <RotationControl v-model="props.selectedElement.rotation" />
-    <ScaleControl v-model="props.selectedElement.scale" />
+    <div :class="[styles.controls]">
+      <LabelControl v-model="props.selectedElement.label" />
+      <PositionControl v-model="props.selectedElement.position" />
+      <RotationControl v-model="props.selectedElement.rotation" />
+      <ScaleControl v-model="props.selectedElement.scale" />
+
+    </div>
+
+    <button @click="whenCloseElementEditor">
+      Close element editor
+    </button>
   </div>
 </template>
 
@@ -14,9 +21,16 @@ import PositionControl from './position-control/position-control.vue';
 import LabelControl from './label-control/label-control.vue';
 import RotationControl from './rotation-control/rotation-control.vue';
 import ScaleControl from './scale-control/scale-control.vue';
+import { useWatermarkElementsStore } from '../../stores/watermark-elements';
 
 const props = defineProps<{
   selectedElement: WatermarkElement
 }>();
+
+const { setSelectedElement } = useWatermarkElementsStore()
+
+const whenCloseElementEditor = () => {
+  setSelectedElement(null)
+}
 
 </script>
